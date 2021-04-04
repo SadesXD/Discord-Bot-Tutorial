42b0fd6fe7a3b678d5090ef1350fd883aa8e6580
SadesXD <68439411+SadesXD@users.noreply.github.com>
2021-04-04 03:12:08 -0700

    Add reverse command

--- commands/reverse.js
+++ commands/reverse.js
@@ -2,14 +2,20 @@ const discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
   
-  let text = args.join(" ");
+  let text = args.join(" "); // Get all argument from the user
   if(!text) {
     return message.channel.send("Please give some text will you reverse !");
   }
   
+
   let reverse = text.split("").reverse().join("");
-  let response = "**" + message.author.tag + ":** " + reverse;
+  /* 
+   This is how to explain reverse variable, example text variable is: "hello world"
+    -> so first we will split the text one by one: text.split(''), the result will be like = ['h','e','l','l','o',' ','w','o','r','l','d']
+    -> then we will reverse the text: .reverse(), the result will be like this: ['d','l','r','o','w',' ','o','l','l','e','h']
+    -> then we will combine all the arrays: .join(''), so the final result will be like this: "dlrow olleh"
+  */
+  let response = "**" + message.author.tag + ":** " + reverse; // result: **SadesXD#3971**: [reverse text]
   
-  return message.channel.send(response)
-  // Done lets test reverse command
+  return message.channel.send(response)// Send the message
 }
